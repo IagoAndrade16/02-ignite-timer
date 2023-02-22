79c07b829fc6a3b6d61c7525d74dd980d94e27a8
Iago Alexandre <iagoaap@outlook.com>
2023-02-22 06:23:05 -0300

    header e layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/index'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 

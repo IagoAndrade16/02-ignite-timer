3c3f1ccc7b8318960a8bfb228f7748c9b8b56fac
Iago Alexandre <iagoaap@outlook.com>
2023-02-24 20:07:09 -0300

    prop drilling

--- src/pages/Home/components/newCycleForm/index.tsx
+++ src/pages/Home/components/newCycleForm/index.tsx
@@ -1,6 +1,28 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
+import { useEffect, useState } from 'react'
+
+type NewCicleFormData = zod.infer<typeof newCicleFormValidationSchema>
 
 export function NewCycleForm() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCicleFormData>({
+    resolver: zodResolver(newCicleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const newCicleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+      .number()
+      .min(1, 'O cicle deve ser no mínimo de 5 minutos')
+      .max(60, 'O ciclo deve ser no máximo de 60 minutos'),
+  })
+
   return (
     <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import { HandPalm, Play } from 'phosphor-react'
-import { useForm } from 'react-hook-form'
 import {
   HomeContainer,
   StartCountdownButton,
@@ -7,21 +6,9 @@ import {
 } from './styles'
 import { differenceInSeconds } from 'date-fns'
 
-import { zodResolver } from '@hookform/resolvers/zod'
-import * as zod from 'zod'
 import { useEffect, useState } from 'react'
 import { NewCycleForm } from './components/newCycleForm'
 import { Countdown } from './components/Countdown'
-
-const newCicleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(1, 'O cicle deve ser no mínimo de 5 minutos')
-    .max(60, 'O ciclo deve ser no máximo de 60 minutos'),
-})
-
-type NewCicleFormData = zod.infer<typeof newCicleFormValidationSchema>
 
 interface ICycle {
   id: string
@@ -35,52 +22,8 @@ interface ICycle {
 export function Home() {
   const [cycles, setCycles] = useState<ICycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
-  const { register, handleSubmit, watch, reset } = useForm<NewCicleFormData>({
-    resolver: zodResolver(newCicleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  })
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
-
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-
-  useEffect(() => {
-    let interval: number
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds(
-          new Date(),
-          activeCycle.startDate,
-        )
-        if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }),
-          )
-
-          setAmountSecondsPassed(totalSeconds)
-
-          clearInterval(interval)
-        } else {
-          setAmountSecondsPassed(secondsDifference)
-        }
-      }, 1000)
-    }
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [activeCycle, totalSeconds, activeCycleId])
 
   function handleCreateNewCicle(data: NewCicleFormData) {
     const newCycle: ICycle = {
@@ -134,7 +77,11 @@ export function Home() {
     <HomeContainer>
       <form action="" onSubmit={handleSubmit(handleCreateNewCicle)}>
         <NewCycleForm />
-        <Countdown />
+        <Countdown
+          activeCycle={activeCycle}
+          setCycles={setCycles}
+          activeCycleId={activeCycleId}
+        />
 
         {activeCycle ? (
           <StopCountdownButton type="button" onClick={handleInterruptCycle}>
